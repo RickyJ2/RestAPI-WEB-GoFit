c0bce4442b80d4178d1255f1b64a7a388cec77ca
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2023-06-05 17:35:13 +0700

    TryInsertDB

--- gofit/database/migrations/2023_03_28_080335_create_informasi_umums_table.php
+++ gofit/database/migrations/2023_03_28_080335_create_informasi_umums_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -21,8 +22,16 @@ return new class extends Migration
             $table->integer('min_deposit_reguler');
             $table->integer('max_deposit_kelas_awal');
         });
-        Artisan::call('db:seed', [
-            '--class' => \Database\Seeders\informasi_umum::class,
+        // Artisan::call('db:seed', [
+        //     '--class' => \Database\Seeders\informasi_umum::class,
+        // ]);
+        DB::table('informasi_umums')->insert([
+            'nama' => 'GoFit',
+            'alamat' => 'Jl. Centralpark No. 10 Yogyakarta',
+            'deskripsi' => 'GoFit adalah studio kebugaran eksklusif di Yogyakarta dengan kelas terjadwal dan fasilitas mandiri. Anggota dapat memesan kelas melalui aplikasi, menggunakan alat-alat kebugaran secara mandiri, dan menikmati fasilitas seperti shower air hangat dan handuk. Aplikasi ini memungkinkan anggota mengelola keanggotaan, jadwal kelas, dan pembayaran dengan mudah.',
+            'biaya_aktivasi_membership' => 3000000,
+            'min_deposit_reguler' => 500000,
+            'max_deposit_kelas_awal' => 0,
         ]);
     }
 
